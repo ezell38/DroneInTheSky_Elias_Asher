380faca1de99cc34768b658250aaff8905bdc871
afriedm49 <afriedm49@charlottesvilleschools.org>
2023-02-07 14:19:02 -0500

    boot w instructions

---
+++ Raspberry-Pi/boot.py
@@ -0,0 +1,16 @@
+import storage
+
+storage.remount("/", False)
+
+# type this in to create test file
+
+# with open("/tmp.txt", "a") as fp:
+# fp.write("hello, world!")
+
+# import os
+# os.listdir("/")
+# os.rename("/boot.py", "/boot.bak")
+
+
+# https://learn.adafruit.com/cpu-temperature-logging-with-circuit-python/writing-to-the-filesystem
+# https://learn.adafruit.com/adafruit-ultimate-gps/circuitpython-datalogging
