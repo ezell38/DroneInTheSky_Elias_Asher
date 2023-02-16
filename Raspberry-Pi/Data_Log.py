92c62917c58e3b31d72e2ddf4ffb0e9c391ccdf0
afriedm49 <afriedm49@charlottesvilleschools.org>
2023-02-16 14:01:08 -0500

    update

--- Raspberry-Pi/Data_Log.py
+++ Raspberry-Pi/Data_Log.py
@@ -9,35 +9,41 @@ import adafruit_gps
 led = digitalio.DigitalInOut(board.LED)     # sets up LED in these 2 lines
 led.direction=digitalio.Direction.OUTPUT 
 
-uart = busio.UART(board.GP4, board.GP5, baudrate=9600, timeout=10)
+uart = busio.UART(board.GP4, board.GP5, baudrate=9600, timeout=10) #
 gps = adafruit_gps.GPS(uart, debug=False) 
 
-gps.latitude=lat
-gps.longitude=lon
+count = 0
+fixes = 0
 
+last_print = time.monotonic()
 while True:
-    gps.update()
-    # Every second print out current location details if there's a fix.
-    current = time.monotonic()
-    if current - last_print >= 0.5:
-        last_print = current
-
-    if not gps.has_fix:
-        # Try again if we don't have a fix yet.
-        print("Waiting for fix...")
-        led.value = False
-        continue
-
-
-
-
     with open("/gps.txt", "a") as fp:
-        while True:
-            temp = microcontroller.cpu.temperature
-            # do the C-to-F conversion here if you would like
-            fp.write(f'{lat}, {lon}\n'.format(gps.latitude))
+        gps.update()
+        # Every second print out current location details if there's a fix.
+        current = time.monotonic()
+        if current - last_print >= 1.0:
+            last_print = current
+            if not gps.has_fix:
+                # Try again if we don't have a fix yet.
+                print("Waiting for fix... (" + count + ", " + fixes + ")")
+                time.sleep(.5)
+                count += 1
+                continue
+            
+            print(gps.latitude)
+            print(gps.longitude)
+
+            count = 0
+            fixes += 1
+
+            fp.write(f'{gps.latitude}, {gps.longitude}\n')
             fp.flush()
 
             led.value = True
             time.sleep(1)
+            led.value = False
+
+           
+   
+        
 
