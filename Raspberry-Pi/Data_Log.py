dc3abe57e4abe55192ff29bd7dc2ec0b48b37416
afriedm49 <55894515+afriedm49@users.noreply.github.com>
2023-03-24 13:35:07 -0400

    Update Data_Log.py

--- Raspberry-Pi/Data_Log.py
+++ Raspberry-Pi/Data_Log.py
@@ -4,37 +4,46 @@ import busio
 import digitalio
 import adafruit_gps
 
-
-
 led = digitalio.DigitalInOut(board.LED)     # sets up LED in these 2 lines
 led.direction=digitalio.Direction.OUTPUT 
 
 uart = busio.UART(board.GP4, board.GP5, baudrate=9600, timeout=10) #
 gps = adafruit_gps.GPS(uart, debug=False) 
 
+prev_lat = 0
+prev_long = 0
+
+gps.send_command(b"PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0")
+gps.send_command(b"PMTK220,500")
 
 
-last_print = time.monotonic()
-with open("/gps.txt", "a") as fp:
+with open("/gps2.txt", "a") as textFile:
     while True:
-        gps.update()
+        update = gps.update()
         # Every second print out current location details if there's a fix.
-        current = time.monotonic()
-        if current - last_print >= 1.0:
-            last_print = current
-            if not gps.has_fix:
-                # Try again if we don't have a fix yet.
-                print("Waiting for fix... ")
-                time.sleep(.5)
-                continue
 
-            print(gps.latitude)
-            print(gps.longitude)    
+        if not gps.has_fix:
+            # Try again if we don't have a fix yet.
+            print("Waiting for fix...")
+            time.sleep(.5)
+            continue
+        else:
+            if update and ((prev_lat != gps.latitude) or (prev_long != gps.longitude)):
+                print(gps.latitude)
+                print(gps.longitude)
+                print(gps.timestamp_utc.tm_min)
+                print(gps.timestamp_utc.tm_sec)
+                print("-" * 40) 
+                
+                textFile.write(f'{gps.latitude}, {gps.longitude}\n')
+                textFile.flush()
+                    
+                prev_lat = gps.latitude
+                prev_long = gps.longitude
 
-            fp.write(f'{gps.latitude}, {gps.longitude}\n')
-            fp.flush()
-
-           
-   
-        
+                led.value = True
+                time.sleep(1)
+                led.value = False
+            else:
+                print("-No new values-")
 
